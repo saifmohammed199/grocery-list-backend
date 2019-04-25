1334a039aa2ecc3e89350663e9e8a6ea76f9d162
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-04-25 08:29:49 +0530

    created server js file and installed pg to connet to the database, created table and inserted values in database and connected the database to the grocery app

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const server = express();
 const port = 3003;
+const { Client } = require('pg')
+const client = new Client()
+
+server.get('/api/list',async (req,res)=>{
+await client.connect()
+const dres = await client.query('SELECT *from grocery_items')
+console.log(dres.rows) 
+await client.end()
+res.send(dres.rows);
+})
+
 
 server.get('/', (req, res) => res.send('Hello World!'))
-server.get('/api/itemList',(req,res)=>res.send("itemList Update soon"))
+server.get('/api/itemList',(req,res)=>res.send('GroceryItems'))
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
